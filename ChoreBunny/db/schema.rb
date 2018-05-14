dbc7fc61db8b7f937861b9fb6fdca603730228aa
Kaari Strack <kaari@absolutedevops.io>
2018-05-14 11:13:52 +0100

    :construction: created category model and established link between category_id and tasks

--- ChoreBunny/db/schema.rb
+++ ChoreBunny/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_14_091922) do
+ActiveRecord::Schema.define(version: 2018_05_14_101243) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -19,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_05_14_091922) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
 end
